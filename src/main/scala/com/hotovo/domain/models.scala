cd94d7d3e5482272820d4c0dd4ec8b8851638bda
ccathlete <ccathlete01@gmail.com>
2025-12-12 14:46:36 +0100

    Created the initial structure of scala files.

--- src/main/scala/com/hotovo/domain/models.scala
+++ src/main/scala/com/hotovo/domain/models.scala
@@ -1,1 +1,31 @@
 package com.hotovo.domain
+
+import io.circe._
+import io.circe.generic.semiauto._
+
+case class ArticleToScrape(id: Int, url: String, source: String)
+
+case class ExtractedAnalysis(
+    headline: String,
+    summary: String,
+    geoEntities: List[String],
+    biasScore: Double
+)
+
+object ExtractedAnalysis {
+  implicit val decoder: Decoder[ExtractedAnalysis] = deriveDecoder
+  implicit val encoder: Encoder[ExtractedAnalysis] = deriveEncoder
+}
+
+case class AnalyzedArticle(
+    articleId: Int,
+    url: String,
+    source: String,
+    headline: String,
+    summary: String,
+    geoEntities: List[String],
+    biasScore: Double,
+    ingestionTimestamp: Long
+)
+
+case class UrlToScrapeQuill(id: Int, url: String, source: String)
