ca7ff3b1ca9182a4695e055d8ab5914b72416b45
ccathlete <ccathlete01@gmail.com>
2025-12-16 14:25:11 +0100

    Added the ZLayer + implementation of the streamScraper service.

--- src/main/scala/com/webcat/hotovo/business/models.scala
+++ src/main/scala/com/webcat/hotovo/business/models.scala
@@ -1,31 +1,3 @@
 package com.webcat.hotovo.business
 
-import io.circe._
-import io.circe.generic.semiauto._
-
-case class ArticleToScrape(id: Int, url: String, source: String)
-
-case class ExtractedAnalysis(
-    headline: String,
-    summary: String,
-    geoEntities: List[String],
-    biasScore: Double
-)
-
-object ExtractedAnalysis {
-  implicit val decoder: Decoder[ExtractedAnalysis] = deriveDecoder
-  implicit val encoder: Encoder[ExtractedAnalysis] = deriveEncoder
-}
-
-case class AnalyzedArticle(
-    articleId: Int,
-    url: String,
-    source: String,
-    headline: String,
-    summary: String,
-    geoEntities: List[String],
-    biasScore: Double,
-    ingestionTimestamp: Long
-)
-
-case class UrlToScrapeQuill(id: Int, url: String, source: String)
+// Domain models placeholder for future use.
