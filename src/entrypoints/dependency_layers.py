4ec7c63f2da5f00c0330a54f3a2c2e5cb15e70af
ccathlete <ccathlete01@gmail.com>
2025-12-20 23:07:15 +0100

    Added cleaning of the chunks before passing to Ollama

--- src/entrypoints/dependency_layers.py
+++ src/entrypoints/dependency_layers.py
@@ -22,7 +22,10 @@ class DataPlatformContainer(containers.DeclarativeContainer):
     # Like ZIO.acquireRelease for the HTTP Client
     http_client = providers.Resource(
         httpx.AsyncClient,
-        timeout=httpx.Timeout(60.0)
+        timeout=httpx.Timeout(60.0),
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        }
     )
 
     # Kafka Producer Resource
